423a54b33557bdf63937dbe153b4bf5e90e0ada6
chrised132 <sanjithchendrika@gmail.com>
2020-03-19 15:55:26 +0800

    route fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,8 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 
 
 const routes: Routes = [
-  { path: 'dashboard', loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule)},
-  { path: '', redirectTo: 'dashboard', pathMatch: 'full'}
+  { path: '', loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule)}
 ];
 
 @NgModule({
